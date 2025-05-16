680ae690e75e9ddfcf0b26a6198448f268de67f6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-16 09:00:12 -0400

    Se le cambio el nombre a la carpeta para expecificar al area que pertenecen

---
+++ Data - Home/Data_Section_7.js
@@ -0,0 +1,46 @@
+
+export const Data_First_Part = [
+    {
+        name: 'Nombre1',
+        Comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima porro, possimus in cumque corporis est?',
+        Star: 4,
+        Img: 'Images/woman-1850703_1920.jpg',
+    },
+    {
+        name: 'Nombre2',
+        Comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima porro, possimus in cumque corporis est?',
+        Star: 5,
+        Img: 'Images/woman-1850703_1920.jpg',
+    },
+];
+
+export const Data_Second_Part = [
+    {
+        name: 'Juan',
+        Comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima porro, possimus in cumque corporis est?',
+        Star: 4,
+        Img: 'Images/woman-1850703_1920.jpg',
+    },
+    {
+        name: 'maria',
+        Comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima porro, possimus in cumque corporis est?',
+        Star: 5,
+        Img: 'Images/woman-1850703_1920.jpg',
+    },
+];
+
+
+export const Data_Three_Part = [
+    {
+        name: 'JuanManuel',
+        Comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima porro, possimus in cumque corporis est?',
+        Star: 4,
+        Img: 'Images/woman-1850703_1920.jpg',
+    },
+    {
+        name: 'mariaDale',
+        Comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima porro, possimus in cumque corporis est?',
+        Star: 5,
+        Img: 'Images/woman-1850703_1920.jpg',
+    },
+];
