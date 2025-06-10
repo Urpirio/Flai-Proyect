e5fbefd80ffb4383d8703ca57ba206e79420ccbd
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-10 09:31:09 -0400

    Formulario

---
+++ Formulario/Prueba.js
@@ -0,0 +1,47 @@
+ const Next1 = () =>{
+      const Parte2Formulario = document.getElementById('Parte2-Formulario');
+      const Parte1Formulario = document.getElementById('Parte-Formulario');
+      Parte1Formulario.classList.replace('d-flex','d-none');
+      Parte2Formulario.classList.replace('d-none','d-flex');
+      const BCarga1 = document.getElementById('BCarga1');
+      BCarga1.style.animation = 'AniCarga1_2 1s';
+      const BCarga2 = document.getElementById('BCarga2');
+      BCarga2.style.animation = 'AniCarga2 1s'
+
+  };
+
+  const Back1 = () =>{
+      const Parte2Formulario = document.getElementById('Parte2-Formulario');
+      const Parte1Formulario = document.getElementById('Parte-Formulario');
+      Parte1Formulario.classList.replace('d-none','d-flex');
+    //   Parte1Formulario.classList.add('d-flex')
+      Parte2Formulario.classList.replace('d-flex','d-none');
+    //   Parte2Formulario.classList.add('d-none')
+      
+      const BCarga1 = document.getElementById('BCarga1');
+      BCarga1.style.animation = 'AniCarga1_1 1s'
+  }
+
+  const Next2 = () => {
+      const Parte3Formulario = document.getElementById('Parte3-Formulario');
+      const Parte2Formulario = document.getElementById('Parte2-Formulario');
+      Parte2Formulario.classList.replace('d-flex','d-none');
+    //   Parte2Formulario.classList.add('d-none');
+      Parte3Formulario.classList.replace('d-none','d-flex');
+    //   Parte3Formulario.classList.add('d-flex');
+      
+      
+
+  };
+
+  const Back2 = () => {
+      
+      const Parte3Formulario = document.getElementById('Parte3-Formulario');
+      const Parte2Formulario = document.getElementById('Parte2-Formulario');
+      Parte2Formulario.classList.replace('d-none','d-flex');
+      Parte3Formulario.classList.replace('d-flex','d-none');
+      const BCarga2 = document.getElementById('BCarga2');
+      BCarga2.style.animation = 'AniCarga3_2 1s';
+      
+      
+  }
